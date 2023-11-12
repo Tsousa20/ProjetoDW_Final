047dfa827e8637d383b1d411cd691e5e2521edb3
kikoanjos <rodrigoanjos10@gmail.com>
2023-11-12 12:53:37 +0000

    Menu e Gallery feitos

---
+++ script.js
@@ -0,0 +1,12 @@
+const btncheck = document.querySelector(".btncheck");
+const navMenu = document.querySelector(".nav_menu");
+
+btncheck.addEventListener("click", () =>{
+    btncheck.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav_link").forEach(n => n.addEventListener("click", () => {
+    btncheck.classList.remove("active");
+    navMenu.classList.remove("active");
+}))
